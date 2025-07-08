b07896b349b98cc7491babda23c47aa0ec8ecb86
aidenpete <kkamatupeter@gmail.com>
2025-07-08 12:38:02 +0300

    set up model and api

--- task_tracker/task_app/models.py
+++ task_tracker/task_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    completed = models.BooleanField(default=False) 
+
+    def __str__(self):
+        return self.title
+
+
+
+
+# CREATE TABLE Task('title', 'completed')
+# VALUES ('Cleaning', 'not done');
